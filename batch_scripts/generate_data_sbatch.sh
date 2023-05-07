1d3ffdca0ed85c65863706913082976b21460070
maryregan <78108025+maryregan@users.noreply.github.com>
2023-05-07 04:59:58 -0400

    batch script edits

--- batch_scripts/generate_data_sbatch.sh
+++ batch_scripts/generate_data_sbatch.sh
@@ -4,6 +4,4 @@
 #SBATCH --mem=32G
 #SBATCH -t 12:00:00
 
-
-echo "going to call python -u ../data/generate.py"
 python -u ../data/generate.py --api_key $APIKEY
--- batch_scripts/train_sbatch.sh
+++ batch_scripts/train_sbatch.sh
@@ -4,6 +4,4 @@
 #SBATCH --mem=64G
 #SBATCH -t 48:00:00
 
-
-echo "going to call python ../model.py --save_weights --num_epochs 1 --max_num_tokens 1024"
-python ../model.py --save_weights --num_epochs 1 --max_num_tokens 1024
+python ../model.py --save_weights "" --num_epochs 10
