a4aeead6486a5f6c64b2d52e8abd4abe6c45af31
maryregan <78108025+maryregan@users.noreply.github.com>
2023-05-07 05:15:31 -0400

    add output files to batch scripts

--- batch_scripts/generate_data_sbatch.sh
+++ batch_scripts/generate_data_sbatch.sh
@@ -3,5 +3,6 @@
 #SBATCH -n 4
 #SBATCH --mem=32G
 #SBATCH -t 12:00:00
+#SBATCH -o slurm-%j.out
 
 python -u ../data/generate.py --api_key $APIKEY
--- batch_scripts/train_sbatch.sh
+++ batch_scripts/train_sbatch.sh
@@ -3,5 +3,6 @@
 #SBATCH -n 8
 #SBATCH --mem=64G
 #SBATCH -t 48:00:00
+#SBATCH -o slurm-%j.out
 
 python ../model.py --save_weights "" --num_epochs 10
